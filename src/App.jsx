7916298e4fcbeef01722dc3d106480ae4b6ba017
vandied <vandiedarling@gmail.com>
2021-03-28 14:31:48 +0300

    add work example

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React, {FC} from "react";
+import scss from './styles/styles.scss'
 
 export const App = () => (
-    <div>
+    <div className={scss.rootApp}>
         Привет, это базовая сборка веб-приложения на реакт
     </div>
 );
